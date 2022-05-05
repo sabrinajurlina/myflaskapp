1cbb4fce858ddf4dafa70f87136a9d5bd6655380
Roberto Hasfura <Alejandro@Robertos-MacBook-Air.local>
2022-05-05 17:31:17 -0500

    day 3 hw - added login & register forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+
 #initializing
 app = Flask(__name__)
 app.config.from_object(Config)
+
+#register plug-ins
+login = LoginManager(app)
+
+#configure some settings
+login.login_view ='login'
+login.login_message = 'You must log in to create your team'
+login.login_message_category = 'warning'
+
+# init my DB manager
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 from app import routes
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+class LoginForm(FlaskForm):
+    email = StringField('Enter Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 class SearchForm(FlaskForm):
-    pokemon = StringField('Pokemon Name', validators=[DataRequired()])
+    pokemon = StringField('Enter Pokemon Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+        validators=[DataRequired(), EqualTo('password', message='Password Must Match')])#must equal password]
+    submit = SubmitField('Register')
+
+    def validate_email(form, field):
+        duplicate_email = User.query.filter_by(email = field.data).first()
+
+        if duplicate_email:
+            raise ValidationError("That email is already registered. Please use another email or select 'Forgot Password' to reset your password")
+            
